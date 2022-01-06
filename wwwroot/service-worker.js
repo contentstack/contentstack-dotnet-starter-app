8f99c0a315b24320ccbb5aa4a39325f8df9e08f5
Uttam Krishna Ukkoji <uttamukkoji@gmail.com>
2022-01-06 16:01:08 +0530

    Live Preview

--- wwwroot/service-worker.js
+++ wwwroot/service-worker.js
@@ -4,17 +4,17 @@
 // be reflected on the first load after each change).
 self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
 
-async function onFetch(event) {
-    let cachedResponse = null;
-    if (event.request.method === 'GET') {
-        // For all navigation requests, try to serve index.html from cache
-        // If you need some URLs to be server-rendered, edit the following check to exclude those URLs
-        const shouldServeIndexHtml = event.request.mode === 'navigate';
+//async function onFetch(event) {
+//    let cachedResponse = null;
+//    if (event.request.method === 'GET') {
+//        // For all navigation requests, try to serve index.html from cache
+//        // If you need some URLs to be server-rendered, edit the following check to exclude those URLs
+//        const shouldServeIndexHtml = event.request.mode === 'navigate';
 
-        const request = shouldServeIndexHtml ? 'index.html' : event.request;
-        const cache = await caches.open(cacheName);
-        cachedResponse = await cache.match(request);
-    }
+//        const request = shouldServeIndexHtml ? 'index.html' : event.request;
+//        const cache = await caches.open(cacheName);
+//        cachedResponse = await cache.match(request);
+//    }
 
-    return cachedResponse || fetch(event.request);
-}
+//    return cachedResponse || fetch(event.request);
+//}
