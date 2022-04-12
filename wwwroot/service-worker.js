495ac20d55d6389c473f1a919551fae5dc9fd86b
Uttam Krishna Ukkoji <uttamukkoji@gmail.com>
2022-04-12 13:11:43 +0530

    SDK update

--- wwwroot/service-worker.js
+++ wwwroot/service-worker.js
@@ -2,7 +2,7 @@
 // In development, always fetch from the network and do not enable offline support.
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
-self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
+//self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
 
 //async function onFetch(event) {
 //    let cachedResponse = null;
